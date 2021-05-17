18e11cda676466c5fe9e26ab8b690704ad3fed9f
bburciaga <burciagab16@gmail.com>
2021-05-16 22:12:42 -0700

    added animated panning to the map and display of latitude and longitude of the map

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import React from 'react';
 import './App.css';
 
 import { MapContainer, TileLayer } from 'react-leaflet';
-import HomePopup from './Components/HomePopup';
 import HomeVectorLayers from './Components/HomeVectorLayers';
-import TrailerParkTitle from './Components/TrailerParkTitle';
+import AnimatedPanning from './Components/AnimatedPanning';
+import LatLngDisplay from './Components/LatLngDisplay';
+
+
 
 function App() {
   return (
     <div className="App mapid">
-      <MapContainer center={[53.91836, -122.64722]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-          <HomeVectorLayers />
-      </MapContainer>
+      <LatLngDisplay />
     </div>
     
   );
--- src/Components/HomeVectorLayers.tsx
+++ src/Components/HomeVectorLayers.tsx
@@ -6,20 +6,16 @@ import {    Circle,
             Popup, 
             LayerGroup, 
             FeatureGroup, 
-            LayersControl,
-            Marker } from 'react-leaflet'
+            LayersControl, 
+            useMap} from 'react-leaflet'
 import { useState } from 'react'
 
 import TrailerParkTitle from './TrailerParkTitle'
-import HomePopup from './HomePopup'
+import { HomePopup, GasStationPopups } from './PrinceGeorgePopups'
+import { useMemo } from 'react'
 
 function HomeVectorLayers() {
 
-    const [trailerParkCenter] = useState({latitude: 53.9177, longitude:-122.64179})
-    const [gasStations] = useState({ stores: [
-        {la:53.92128,lo:-122.6483},
-        {la:53.90608,lo:-122.67687}
-    ]})
     //la = latitude; lo = longitude
     //tpS stands for trailer park square
     //crnrs = corners
@@ -27,6 +23,32 @@ function HomeVectorLayers() {
         {la:53.91995,lo:-122.6411},
         {la:53.91681,lo:-122.64278}
     ]})
+    const [outertpS] = useState({ crnrs: [
+        {la:53.91999,lo:-122.64219},
+        {la:53.91667,lo:-122.64385}
+    ]})
+
+    const [bounds, setBounds] = useState(outertpS)
+    const map = useMap()
+
+    const innerHandlers = useMemo(
+        () => ({
+            click() {
+                setBounds(outertpS)
+                map.fitBounds([
+                    [outertpS.crnrs[0].la,outertpS.crnrs[0].lo],
+                    [outertpS.crnrs[1].la,outertpS.crnrs[1].lo]
+                ])
+            },
+        }),
+        [map],
+    )
+
+    const [locations] = useState({ stores: [
+        {la:53.89658,lo:-122.64176},
+        {la:53.90691,lo:-122.72378}
+    ]})
+    
     //tpP stands for trailer park polyline
     //rd = road
     const [tpP] = useState({rd: [
@@ -57,34 +79,40 @@ function HomeVectorLayers() {
                             ]} />
                         <Rectangle
                             pathOptions={fillBlackOption}
+                            eventHandlers={innerHandlers}
                             bounds={[
                                 [tpS.crnrs[0].la,tpS.crnrs[0].lo],
                                 [tpS.crnrs[1].la,tpS.crnrs[1].lo]
                             ]} />
                         <TrailerParkTitle />
-                        <HomePopup />
                     </LayerGroup>
                 </LayersControl.Overlay>
 
-                <LayersControl.Overlay checked name="Layer of Gas Stations">
+                <LayersControl.Overlay checked name="Layer of Locations">
                     <FeatureGroup>
                         <Circle
-                        center={[gasStations.stores[0].la, gasStations.stores[0].lo]}
+                        center={[locations.stores[0].la, locations.stores[0].lo]}
                         pathOptions={purpleOptions} radius={100}>
                             <Popup>
-                                Airport Husky
+                                Blackburn Skatepark
                             </Popup>
                         </Circle>
                         <Circle
-                        center={[gasStations.stores[1].la, gasStations.stores[1].lo]}
+                        center={[locations.stores[1].la, locations.stores[1].lo]}
                         pathOptions={purpleOptions} radius={100}>
                             <Popup>
-                                Petro Canada
+                                LG Gunn Park
                             </Popup>
                         </Circle>
                     </FeatureGroup>
                 </LayersControl.Overlay>
-
+                
+                <LayersControl.Overlay checked name="Layer of Gas Station Popups">
+                    <GasStationPopups />
+                </LayersControl.Overlay>
+                <LayersControl.Overlay checked name="Layer of Current Position">
+                    <HomePopup />
+                </LayersControl.Overlay>
             </LayersControl>
 
             
